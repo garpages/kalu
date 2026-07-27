9a953f569b16e28f81c0e81142cf42282a6c26ca
mahdi <babamahdi548@gmail.com>
2026-07-27 10:08:02 +0330

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,9 @@
 import type { NextConfig } from "next";
+
 const nextConfig: NextConfig = {};
+
 export default nextConfig;
+
+// Enables local dev support for bindings defined in wrangler.jsonc
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+initOpenNextCloudflareForDev();
